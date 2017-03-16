9f5553c24a0a0a97a3b70f0dd7a28a688fa82a59
Arthur Costa Lopes <arthurlopes094@gmail.com>
2017-03-16 19:37:01 -0300

    ADDED: perf commands

--- linux-4.10.3/run.sh
+++ linux-4.10.3/run.sh
@@ -4,4 +4,5 @@ cp ../config .config
 echo "make clean"
 make clean
 echo "make -j$(cat /proc/cpuinfo | grep proc | wc -l)"
-make -j$(cat /proc/cpuinfo | grep proc | wc -l)
+perf stat -d make -j$(cat /proc/cpuinfo | grep proc | wc -l) 2> stat
+perf stat -d -r 2 make -j$(cat /proc/cpuinfo | grep proc | wc -l) 2>> stat
